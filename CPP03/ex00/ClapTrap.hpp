b379a8a708ac264e5603b35dfc3b91c21c7f98b8
Otmane Haimad <ohaimad@e1r5p8.1337.ma>
2023-12-23 04:15:11 +0100

    done

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ohaimad <ohaimad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 03:59:32 by ohaimad           #+#    #+#             */
+/*   Updated: 2023/12/23 04:08:40 by ohaimad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<cmath>
+#pragma once
+
+class ClapTrap {
+private:
+    std::string Name;
+    unsigned int HitPoints;
+    unsigned int EnergyPoints;
+    unsigned int AttackDamage;
+
+public:
+    // Constructor
+    ClapTrap(const std::string& name);
+
+    // Member functions
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+
+    // Getter functions
+    std::string getName() const;
+    unsigned int getHitPoints() const;
+    unsigned int getEnergyPoints() const;
+    unsigned int getAttackDamage() const;
+};
+
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -6,10 +6,29 @@
 /*   By: ohaimad <ohaimad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:16:22 by ohaimad           #+#    #+#             */
-/*   Updated: 2023/12/20 01:36:44 by ohaimad          ###   ########.fr       */
+/*   Updated: 2023/12/23 04:08:05 by ohaimad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+#include "claptrap.hpp"
 
+int main() {
+    // Create a ClapTrap instance
+    ClapTrap claptrap("CL4P-TP");
+
+    // Test attack, takeDamage, and beRepaired
+    claptrap.attack("Enemy1");
+    claptrap.takeDamage(5);
+    claptrap.beRepaired(3);
+
+    // Display current status
+    std::cout << "Current Status:" << std::endl;
+    std::cout << "Name: " << claptrap.getName() << std::endl;
+    std::cout << "Hit Points: " << claptrap.getHitPoints() << std::endl;
+    std::cout << "Energy Points: " << claptrap.getEnergyPoints() << std::endl;
+    std::cout << "Attack Damage: " << claptrap.getAttackDamage() << std::endl;
+
+    return 0;
+}
