cbb709670d4c92abb03484e17c1e5e05e2361a23
Otmane <otmane@Designs-iMac.local>
2024-01-25 23:24:39 +0100

    done

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ohaimad <ohaimad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 20:39:52 by ohaimad           #+#    #+#             */
-/*   Updated: 2024/01/25 23:02:10 by ohaimad          ###   ########.fr       */
+/*   Updated: 2024/01/25 23:23:52 by ohaimad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 int main() {
     try {
-        Bureaucrat bureaucrat("PAQUETA", 50);
+        Bureaucrat bureaucrat("PAQUETA", 2);
         std::cout << bureaucrat << std::endl;
 
         bureaucrat.incrementGrade();
-        std::cout << bureaucrat << std::endl;
-
-        bureaucrat.decrementGrade();
         std::cout << bureaucrat << std::endl;
 
         // This will throw GradeTooHighException
         bureaucrat.incrementGrade();
+
+        bureaucrat.decrementGrade();
+        std::cout << bureaucrat << std::endl;
     }
     catch (const std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
