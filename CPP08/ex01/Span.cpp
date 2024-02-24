5a966f6403c05657fa36aaf353299a5d0c27effa
Otmane Haimad <ohaimad@e1r5p9.1337.ma>
2024-02-24 02:43:34 +0100

    regl

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: ohaimad <ohaimad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:11:13 by ohaimad           #+#    #+#             */
-/*   Updated: 2024/02/20 13:13:06 by ohaimad          ###   ########.fr       */
+/*   Updated: 2024/02/24 02:42:52 by ohaimad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,18 @@ int Span::shortestSpan() const {
     if (numbers.size() <= 1)
         throw std::out_of_range("Cannot find span with less than 2 numbers");
 
+    std::vector<int> sortedNumbers = numbers;
+    std::sort(sortedNumbers.begin(), sortedNumbers.end());
+
     int shortest = std::numeric_limits<int>::max();
-    for (size_t i = 0; i < numbers.size() - 1; ++i) {
-        int span = numbers[i + 1] - numbers[i];
+    for (size_t i = 0; i < sortedNumbers.size() - 1; ++i) {
+        int span = sortedNumbers[i + 1] - sortedNumbers[i];
         if (span < shortest)
             shortest = span;
     }
     return shortest;
 }
+
 
 int Span::longestSpan() const {
     if (numbers.size() <= 1)
