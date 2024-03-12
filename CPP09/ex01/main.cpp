de81fdd982879b7416dbf2df0408df74d24b0959
Otmane Haimad <ohaimad@e1r5p6.1337.ma>
2024-03-12 01:43:56 +0000

    01

---
+++ CPP09/ex01/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ohaimad <ohaimad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 11:12:02 by ohaimad           #+#    #+#             */
+/*   Updated: 2024/03/12 01:28:44 by ohaimad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char* av[])
+{
+    if (ac != 2)
+    {
+        std::cerr << "Error: could not open file. "<< std::endl;
+        return 1;
+    }
+
+    // std::string inputFile(av[1]);
+
+    BitcoinExchange bitcoinExchange;
+    bitcoinExchange.processInputFile(av[1]);
+
+    return 0;
+}
