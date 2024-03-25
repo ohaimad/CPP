1af6cd8f0174b7cae56f1356eac86ade677266cc
Otmane Haimad <ohaimad@e1r5p2.1337.ma>
2024-03-25 02:56:05 +0000

    notdone

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -1,7 +1,23 @@
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange()
-{}
+{
+    const std::string databaseFilePath = "data.csv";
+    std::ifstream file(databaseFilePath);
+    std::string line;
+    std::getline(file, line);
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        std::string date;
+        float value;
+
+        if (std::getline(iss, date, ',') && iss >> value)
+                exchangeRates[date] = value;
+           
+    }
+    file.close();
+}
 
 int count_points(std::string value)
 {
@@ -16,9 +32,17 @@ int count_points(std::string value)
 
 bool pars_value(std::string value)
 {
+    std::cout << "value: " << value << std::endl;
+    if(value[0] != ' ')
+    {
+        std::cerr << "Error: bad value => " << value << std::endl;
+        return false;
+    }
     for(size_t i = 0; i < value.size(); i++)
     {
-        if(count_points(value) > 1)
+        if( i == 0)
+            i++;
+        if(count_points(value) > 1 || (!std::isdigit(value[i]) && value[i] != '.' && value[i] != '+'))
         {
             std::cerr << "Error: bad value => " << value << std::endl;
             return false;
@@ -27,7 +51,7 @@ bool pars_value(std::string value)
         {
             if(i == 0 || i == value.size() - 1)
             {
-                std::cerr << "Error: bad value => " << value << std::endl;
+                std::cerr << "--Error: bad value => " << value << std::endl;
                 return false;
             }
         }
@@ -45,7 +69,6 @@ void BitcoinExchange::processInputFile(const std::string& inputFile)
     }
 
     std::string line;
-    std::string line_trime;
     std::getline(file, line);
     if (line != "date | value")
     {
@@ -59,9 +82,8 @@ void BitcoinExchange::processInputFile(const std::string& inputFile)
         std::string value;
         float exchangeRate;
 
-        if (std::getline(iss, date, ' '))
+        if (std::getline(iss, date, '|'))
         {
-            std::cout << "date: " << date << std::endl;
             if (isValidDate(date))
             {
                 std::string closestDate = getClosestDate(date);
@@ -69,79 +91,64 @@ void BitcoinExchange::processInputFile(const std::string& inputFile)
             }
             else
             {
-                std::cerr << "Error: bad date => " << date << std::endl;
-                return;
+                std::cerr << "Error: bad input => " << date << std::endl;
+                continue;
             }
         }
         if (std::getline(iss, value) && pars_value(value))
         {
+            std::cout << "value: " << value << std::endl;
             float value_f;
             std::istringstream(value) >> value_f;
-            std::cout << "value: " << value_f << std::endl;
-            
+            std::cout << "value_f: " << value_f << std::endl;
             if(!isValidValue(value_f))
-                std::cerr << "Error: bad value_f => " << value_f << std::endl;
+                continue;
             else
             {
                 float result = value_f * exchangeRate;
                 std::cout << date << " => " << value_f << " = " << result << std::endl;
             }
         }
-        file.close();
     }
+    file.close();
 }
 
 bool BitcoinExchange::isValidDate(const std::string& date) const 
 {
-    std::cout << "date size : " << date.size() << std::endl;
+    std::cout << "datesize: " << date.size() << std::endl;
     int year = (date[0] - '0') * 1000 + (date[1] - '0') * 100 + (date[2] - '0') * 10 + (date[3] - '0');
     int month = (date[5] - '0') * 10 + (date[6] - '0');
     int day = (date[8] - '0') * 10 + (date[9] - '0');
 
-    if (date.size() != 10) {
-        puts("Date size is incorrect.");
+    if (date.size() != 11)
         return false;
-    }
     if (!std::isdigit(date[0]) || !std::isdigit(date[1]) ||
         !std::isdigit(date[2]) || !std::isdigit(date[3]) ||
         date[4] != '-' || !std::isdigit(date[5]) || 
         !std::isdigit(date[6]) || date[7] != '-' || !std::isdigit(date[8]) || !std::isdigit(date[9]))
     {
-        puts("Date format is incorrect.");
         return false;
     }
 
-    if (month < 1 || month > 12) {
-        puts("Month is out of range.");
+    if (month < 1 || month > 12)
         return false;
-    }
-    if (day < 1 || day > 31) {
-        puts("Day is out of range.");
+    if (day < 1 || day > 31) 
         return false;
-    }
     if (month == 2) 
     {
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
         {
             if (day > 29) 
-            {
-                puts("Day is out of range for February (leap year).");
                 return false;
-            }
         } 
         else 
         {
             if (day > 28) 
-            {
-                puts("Day is out of range for February.");
                 return false;
-            }
         }
     } 
-    else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
-        puts("Day is out of range for this month.");
+    else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) 
         return false;
-    }
     return true;
 }
 
@@ -165,17 +172,20 @@ std::string BitcoinExchange::getClosestDate(const std::string& date) const
 {
         std::map<std::string, float>::const_iterator it = exchangeRates.lower_bound(date);
         if (it == exchangeRates.begin())
-            return it->first;
+        {
+                std::cout<< "it->first: " << it->first << std::endl;
+                return it->first;
+        }
         else if (it == exchangeRates.end())
         {
             std::map<std::string, float>::const_iterator temp = it;
-            --temp;
+            temp--;
             return temp->first;
         }
         else
         {
-            --it;
-            return it->first; 
+            it--;
+            return it->first;
         }
     return "";
 }
